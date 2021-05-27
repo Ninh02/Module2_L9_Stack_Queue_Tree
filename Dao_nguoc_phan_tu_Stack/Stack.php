fa21ef9fe088d4ff43d17bb9aac4af626b8cb304
Ninh02 <ninhdo10021998@gmail.com>
2021-05-27 13:22:29 +0700

    bai tap

--- Dao_nguoc_phan_tu_Stack/Stack.php
+++ Dao_nguoc_phan_tu_Stack/Stack.php
@@ -1,1 +1,32 @@
 <?php
+class Stack
+{
+    public array $stack;
+    public int $n;
+    public function __construct($stack=[],$n)
+    {
+        $this->stack=$stack;
+        $this->n=$n;
+    }
+    public function push($item)
+    {
+        if (count($this->stack) < $this->n) {
+            array_unshift($this->stack, $item);
+        } else {
+            echo "stack is full";
+        }
+    }
+    public function isEmpty(): bool
+    {
+        return empty($this->stack);
+    }
+
+    public function pop()
+    {
+            $arr=[];
+           for($i=count($this->stack)-1;$i>=0;$i--){
+               array_push($arr,$this->stack[$i]);
+           }
+           return $arr;
+    }
+}
--- Dao_nguoc_phan_tu_Stack/index.php
+++ Dao_nguoc_phan_tu_Stack/index.php
@@ -1,1 +1,12 @@
 <?php
+include_once "Stack.php";
+$stack=new Stack([],4);
+$stack->push("Đắc nhân tâm ");
+$stack->push("Bong bóng lên trời ");
+$stack->push("Hai số phận ");
+$stack->push("Búp sen xanh ");
+echo "<pre>";
+print_r($stack);
+echo "<pre>";
+print_r($stack->pop());
+
